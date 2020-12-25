e5229f6e3f96a29d08c91551e0ed8cde01402092
poloskov <poloskovcg@gmail.com>
2020-12-25 20:44:30 +0300

    deploy

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
 
+EXT_IP = '34.90.19.13'
+INT_IP = '10.164.0.2'
+
+
 ADMIN_PASSWORD = 'admin'
-TOKEN = '1042488699:AAFEsvgJXyEIR3REXrSAMZjQB-HClDnNE0U'
+
+# # test bot
+# TOKEN = '1042488699:AAFEsvgJXyEIR3REXrSAMZjQB-HClDnNE0U'
+
+
+# main bot
+TOKEN = '1490631920:AAEw5BTuUsRElSGpDAaYbmS8BK1aOmmR0vo'
 
 WEBHOOK_LISTEN = "localhost"
 WEBHOOK_PORT = 80
 
-WEBHOOK_SSL_CERT = "/home/tg_tink/ssl2/ssl.pem"
-WEBHOOK_SSL_PRIV = "/home/tg_tink/ssl2/ssl_priv.pem"
+WEBHOOK_SSL_CERT = "/home/rent_b/ssl/ssl.pem"
+WEBHOOK_SSL_PRIV = "/home/rent_b/ssl/ssl_priv.pem"
 
 IMAGE_FOLDER = 'photos'
 
@@ -17,7 +27,7 @@ USER_DATA_OLD_TIME = 60 # sec
 DB_USER = 'postgres'
 DB_PASSWORD = 'SF899laxe_'
 DB_DATABASE = 'rent'
-DB_HOST = '34.90.19.13'
+DB_HOST = 'localhost'
 
 FILTER_APP_HOST = 'localhost'
 FILTER_APP_PORT = 82
