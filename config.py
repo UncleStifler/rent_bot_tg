430aacdd2e34fb843cb94dfad75385ea74b2a45b
poloskov <poloskovcg@gmail.com>
2020-12-25 21:03:26 +0300

    deploy

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ INT_IP = '10.164.0.2'
 
 ADMIN_PASSWORD = 'admin'
 
-# # test bot
-# TOKEN = '1042488699:AAFEsvgJXyEIR3REXrSAMZjQB-HClDnNE0U'
+# test bot
+TOKEN = '1042488699:AAFEsvgJXyEIR3REXrSAMZjQB-HClDnNE0U'
 
 
-# main bot
-TOKEN = '1490631920:AAEw5BTuUsRElSGpDAaYbmS8BK1aOmmR0vo'
+# # main bot
+# TOKEN = '1490631920:AAEw5BTuUsRElSGpDAaYbmS8BK1aOmmR0vo'
 
 WEBHOOK_LISTEN = INT_IP
-WEBHOOK_PORT = 80
+WEBHOOK_PORT = 8443
 
 WEBHOOK_SSL_CERT = "/home/rent_b/ssl/ssl.pem"
 WEBHOOK_SSL_PRIV = "/home/rent_b/ssl/ssl_priv.pem"
@@ -27,7 +27,7 @@ USER_DATA_OLD_TIME = 60 # sec
 DB_USER = 'postgres'
 DB_PASSWORD = 'SF899laxe_'
 DB_DATABASE = 'rent'
-DB_HOST = INT_IP
+DB_HOST = 'localhost'
 
 FILTER_APP_HOST = INT_IP
 FILTER_APP_PORT = 82
